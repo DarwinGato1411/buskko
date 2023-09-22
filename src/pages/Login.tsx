840b080f0b3b01313d3a32fbfd99c85d804a9e68
NanoDan29 <jowell29jm@gmail.com>
2023-09-21 23:51:25 -0500

    creacion apis

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -3,20 +3,78 @@ import { IonPage, IonContent, IonInput, IonButton, IonIcon } from '@ionic/react'
 import '../styles/Login.scss'
 import { personCircleOutline, keyOutline } from 'ionicons/icons';
 import buskkoLogo from '../assets/img/BUSKKO.png'
-import { useHistory } from 'react-router-dom';
+import { Redirect, useHistory } from 'react-router-dom';
+import { loginCliente } from '../api/login'
+import Swal from 'sweetalert2';
 
+interface UserCredentials {
+    usuLogin: string;
+    usuPassword: string;
+}
+
+interface ApiResponse {
+    status: number;
+    message: string;
+    data: UserData | null;
+}
+
+interface UserData {
+    usuWhatsapp: string,
+    usuActivaMovil: string,
+    usuFechaRegMov: string,
+    usuFechaCaduca: string,
+    usuDireccion: string,
+    idUsuario: string,
+    usuNombre: string,
+    usuLogin: string,
+    usuPassword: string,
+    usuCorreo: string,
+    usuNivel: number,
+    usuRuc: string,
+    usuFechaRegistro: Date
+}
 const LoginPage: React.FC = () => {
-    const [email, setEmail] = useState('');
+    const [userName, setUserName] = useState('');
     const [password, setPassword] = useState('');
+    const [loggedIn, setLoggedIn] = useState(false);
     const history = useHistory();
 
-    const handleLogin = () => {
-        window.location.href = '/tienda';
+    const handleLogin = async () => {
+        const userCredentials: UserCredentials = { usuLogin: userName, usuPassword: password };
+        try {
+            const respuesta = await loginCliente(userCredentials);
+            if (respuesta.status === 200) {
+                if (respuesta.data && respuesta.data.usuRuc != null) {
+                    localStorage.setItem('usuario', JSON.stringify(respuesta.data));
+                    setLoggedIn(true);
+                    Swal.fire(
+                        'Login exitoso',
+                        'Usuario logeado con exito',
+                        'success'
+                    )
+                } else {
+                    Swal.fire(
+                        'Atención',
+                        'El usuario no está registrado',
+                        'error'
+                    )
+                }
+
+            } else {
+                console.error('Error en la solicitud:', respuesta.status);
+            }
+        } catch (error: any) {
+            console.error('Error al iniciar sesión:', error.message);
+        }
     };
 
     const handleNavigate = () => {
         history.push('/registro'); // Cambia '/otra-pagina' a la ruta de la otra página
     };
+
+    if (loggedIn) {
+        return <Redirect to="/tienda" />;
+    }
 
     return (
         <IonPage>
@@ -36,8 +94,8 @@ const LoginPage: React.FC = () => {
                                 <IonInput
                                     type="email"
                                     placeholder="Correo Electrónico"
-                                    value={email}
-                                    onIonChange={(e) => setEmail(e.detail.value!)}
+                                    value={userName}
+                                    onIonChange={(e) => setUserName(e.detail.value!)}
 
                                 ></IonInput>
                             </div>
